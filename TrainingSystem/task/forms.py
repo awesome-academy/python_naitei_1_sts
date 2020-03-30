e972820a1f490ac599b83c2ab5d9f1b9aa572a10
kentranptit <kentran24797@gmail.com>
2020-03-30 16:47:50 +0700

    create datepicker widget for adding new task

--- TrainingSystem/task/forms.py
+++ TrainingSystem/task/forms.py
@@ -1,11 +1,22 @@
 from django import forms
+from django.conf import settings
 from .models import Task
+from .widgets import DatePickerInput
 
 class TaskCreateForm(forms.ModelForm):
+    start_date = forms.DateField(
+        input_formats=settings.DATE_INPUT_FORMATS,
+        widget=DatePickerInput()
+    )
+    due_date = forms.DateField(
+        input_formats=settings.DATE_INPUT_FORMATS,
+        widget=DatePickerInput()
+    )
     class Meta:
         model = Task
         fields = '__all__'
         exclude = ('creator',)
+
 
 class TaskUpdateForm(forms.ModelForm):
     class Meta:
